a9a6a0c473b819695cb7db9a3cb500bb61da4171
AGORIOLAUTARO <lautaroagorio@gmail.com>
2026-03-29 20:32:22 -0300

    29-03

--- pryRossiLabII/Form1.Designer.cs
+++ pryRossiLabII/Form1.Designer.cs
@@ -33,9 +33,9 @@
             this.txtNombre = new System.Windows.Forms.TextBox();
             this.lblNombre = new System.Windows.Forms.Label();
             this.grbListar = new System.Windows.Forms.GroupBox();
-            this.btnWhile = new System.Windows.Forms.Button();
-            this.lstPersonas = new System.Windows.Forms.ListBox();
             this.btnFor = new System.Windows.Forms.Button();
+            this.lstPersonas = new System.Windows.Forms.ListBox();
+            this.btnWhile = new System.Windows.Forms.Button();
             this.grbDatos.SuspendLayout();
             this.grbListar.SuspendLayout();
             this.SuspendLayout();
@@ -46,19 +46,22 @@
             this.grbDatos.Controls.Add(this.txtNombre);
             this.grbDatos.Controls.Add(this.lblNombre);
             this.grbDatos.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grbDatos.Location = new System.Drawing.Point(12, 25);
+            this.grbDatos.Location = new System.Drawing.Point(16, 31);
+            this.grbDatos.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grbDatos.Name = "grbDatos";
-            this.grbDatos.Size = new System.Drawing.Size(302, 142);
+            this.grbDatos.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grbDatos.Size = new System.Drawing.Size(403, 175);
             this.grbDatos.TabIndex = 0;
             this.grbDatos.TabStop = false;
             this.grbDatos.Text = "Carga de Datos";
             // 
             // brnCargar
             // 
-            this.brnCargar.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.brnCargar.Location = new System.Drawing.Point(194, 82);
+            this.brnCargar.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.brnCargar.Location = new System.Drawing.Point(259, 101);
+            this.brnCargar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.brnCargar.Name = "brnCargar";
-            this.brnCargar.Size = new System.Drawing.Size(93, 28);
+            this.brnCargar.Size = new System.Drawing.Size(124, 34);
             this.brnCargar.TabIndex = 2;
             this.brnCargar.Text = "Cargar";
             this.brnCargar.UseVisualStyleBackColor = true;
@@ -66,17 +69,19 @@
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(129, 34);
+            this.txtNombre.Location = new System.Drawing.Point(172, 42);
+            this.txtNombre.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNombre.Name = "txtNombre";
-            this.txtNombre.Size = new System.Drawing.Size(167, 22);
+            this.txtNombre.Size = new System.Drawing.Size(221, 26);
             this.txtNombre.TabIndex = 1;
             // 
             // lblNombre
             // 
             this.lblNombre.AutoSize = true;
-            this.lblNombre.Location = new System.Drawing.Point(22, 37);
+            this.lblNombre.Location = new System.Drawing.Point(29, 46);
+            this.lblNombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNombre.Name = "lblNombre";
-            this.lblNombre.Size = new System.Drawing.Size(107, 16);
+            this.lblNombre.Size = new System.Drawing.Size(142, 22);
             this.lblNombre.TabIndex = 0;
             this.lblNombre.Text = "Nombre y Apellido:";
             // 
@@ -86,52 +91,58 @@
             this.grbListar.Controls.Add(this.lstPersonas);
             this.grbListar.Controls.Add(this.btnWhile);
             this.grbListar.Font = new System.Drawing.Font("Arial Narrow", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.grbListar.Location = new System.Drawing.Point(12, 186);
+            this.grbListar.Location = new System.Drawing.Point(16, 229);
+            this.grbListar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.grbListar.Name = "grbListar";
-            this.grbListar.Size = new System.Drawing.Size(302, 235);
+            this.grbListar.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.grbListar.Size = new System.Drawing.Size(403, 289);
             this.grbListar.TabIndex = 3;
             this.grbListar.TabStop = false;
             this.grbListar.Text = "Listado de Datos";
             // 
-            // btnWhile
-            // 
-            this.btnWhile.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnWhile.Location = new System.Drawing.Point(173, 165);
-            this.btnWhile.Name = "btnWhile";
-            this.btnWhile.Size = new System.Drawing.Size(93, 51);
-            this.btnWhile.TabIndex = 2;
-            this.btnWhile.Text = "Listar con While";
-            this.btnWhile.UseVisualStyleBackColor = true;
-            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
-            // 
-            // lstPersonas
-            // 
-            this.lstPersonas.FormattingEnabled = true;
-            this.lstPersonas.ItemHeight = 16;
-            this.lstPersonas.Location = new System.Drawing.Point(49, 33);
-            this.lstPersonas.Name = "lstPersonas";
-            this.lstPersonas.Size = new System.Drawing.Size(214, 116);
-            this.lstPersonas.TabIndex = 3;
-            // 
             // btnFor
             // 
-            this.btnFor.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnFor.Location = new System.Drawing.Point(49, 165);
+            this.btnFor.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFor.Location = new System.Drawing.Point(44, 191);
+            this.btnFor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnFor.Name = "btnFor";
-            this.btnFor.Size = new System.Drawing.Size(93, 51);
+            this.btnFor.Size = new System.Drawing.Size(138, 40);
             this.btnFor.TabIndex = 4;
             this.btnFor.Text = "Listar con For";
             this.btnFor.UseVisualStyleBackColor = true;
             this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
             // 
+            // lstPersonas
+            // 
+            this.lstPersonas.FormattingEnabled = true;
+            this.lstPersonas.ItemHeight = 20;
+            this.lstPersonas.Location = new System.Drawing.Point(44, 41);
+            this.lstPersonas.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.lstPersonas.Name = "lstPersonas";
+            this.lstPersonas.Size = new System.Drawing.Size(305, 124);
+            this.lstPersonas.TabIndex = 3;
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Font = new System.Drawing.Font("Arial", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnWhile.Location = new System.Drawing.Point(201, 191);
+            this.btnWhile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(148, 40);
+            this.btnWhile.TabIndex = 2;
+            this.btnWhile.Text = "Listar con While";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
             // frmVector
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.ClientSize = new System.Drawing.Size(337, 450);
+            this.ClientSize = new System.Drawing.Size(449, 554);
             this.Controls.Add(this.grbListar);
             this.Controls.Add(this.grbDatos);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmVector";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Vectores";
